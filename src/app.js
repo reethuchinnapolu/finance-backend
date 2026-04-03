22ffa1b816ceef9ec19a907cd9d868585b0b4493
reethuchinnapolu <reethureddy0407@gmail.com>
2026-04-03 18:51:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,9 +10,23 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/records", require("./routes/recordRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB connected");
-    app.listen(5000, () => console.log("Server running"));
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   })
   .catch(err => console.log(err));
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message || "Server Error"
+  });
+});
