592d32e5b87d789667791fd06ec2876b9e29dd0d
reethuchinnapolu <reethureddy0407@gmail.com>
2026-04-03 18:43:55 +0530

    Delete routes directory

--- routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { getSummary } = require("../controllers/dashboardController");
-const { authenticate } = require("../middleware/auth");
-
-router.get("/summary", authenticate, getSummary);
-
-module.exports = router;
